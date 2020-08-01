a4c8775156e26ce2702a76b0929c7177ea69761b
wenchangliu <>
2020-08-01 15:10:30 +0200

    vince: prop: enable QC2 bufferqueue block-pool
    Enable Qualcomm codec 2.0 bufferqueue block-pool for
    reducing 10% of power consumption on 1080p@30fps video playback
    
    Test: Full video manual test
    Signed-off-by: ashwatthama <sai404142@gmail.com>
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- prop.mk
+++ prop.mk
@@ -245,3 +245,7 @@ persist.sys.wfd.virtual=0
 # Zygote preforking
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.device_config.runtime_native.usap_pool_enabled=true
+
+# Enable QC2 bufferqueue block-pool
+PRODUCT_PROPERTY_OVERRIDES += \
+vendor.qc2.use.bqpool=1
