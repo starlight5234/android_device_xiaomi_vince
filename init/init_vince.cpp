38fe9d45675bcaa6920a383aef8db347c2743e44
DennySPB <dennyspb@gmail.com>
2020-09-16 14:42:02 +0530

    vince: Set prop ro.control_privapp_permissions=log
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- init/init_vince.cpp
+++ init/init_vince.cpp
@@ -28,6 +28,11 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <sys/sysinfo.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <android-base/properties.h>
+#include <sys/_system_properties.h>
+#include <stdio.h>
+#include <sys/system_properties.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
@@ -40,6 +45,7 @@ char const *heapminfree;
 char const *heapmaxfree;
 char const *heaptargetutilization;
 
+using android::base::GetProperty;
 using android::init::property_set;
 
 void check_device()
@@ -67,6 +73,24 @@ void check_device()
    }
 }
 
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void property_override_dual(char const system_prop[], char const vendor_prop[],
+    char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
+
 void vendor_load_properties()
 {
     check_device();
@@ -77,4 +101,5 @@ void vendor_load_properties()
     property_set("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_set("dalvik.vm.heapminfree", heapminfree);
     property_set("dalvik.vm.heapmaxfree", heapmaxfree);
+    property_override_dual("ro.control_privapp_permissions", "ro.control_privapp_permissions", "log");
 }
